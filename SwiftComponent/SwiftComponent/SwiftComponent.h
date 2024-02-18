cca7b3c289ee5a058e82ab3d2378b1d7044edb59
lijingfu <lijingfu@zsxq.com>
2024-02-18 15:18:31 +0800

    framework

---
+++ SwiftComponent/SwiftComponent/SwiftComponent.h
@@ -0,0 +1,18 @@
+//
+//  SwiftComponent.h
+//  SwiftComponent
+//
+//  Created by Jingfu Li on 2024/2/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftComponent.
+FOUNDATION_EXPORT double SwiftComponentVersionNumber;
+
+//! Project version string for SwiftComponent.
+FOUNDATION_EXPORT const unsigned char SwiftComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftComponent/PublicHeader.h>
+
+
