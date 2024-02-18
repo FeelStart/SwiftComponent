8797469152323913b0faeb1ac28665382a2675cd
lijingfu <lijingfu@zsxq.com>
2024-02-18 15:30:25 +0800

    example

---
+++ SwiftComponentExample/SwiftComponentExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftComponentExample
+//
+//  Created by Jingfu Li on 2024/2/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
