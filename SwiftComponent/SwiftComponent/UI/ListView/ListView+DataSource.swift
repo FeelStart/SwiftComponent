29dd8450f3b92160a4046210b325b155ddbd6ee1
lijingfu <lijingfu@zsxq.com>
2024-02-19 09:25:26 +0800

    ListView: 协议

--- SwiftComponent/SwiftComponent/UI/ListView/ListView+DataSource.swift
+++ SwiftComponent/SwiftComponent/UI/ListView/ListView+DataSource.swift
@@ -8,17 +8,24 @@
 import UIKit
 
 public protocol ListViewSection {
+    var header: ListViewHeader? { get set }
+    var footer: ListViewFooter? { get set }
     var rows: [ListViewRow] { get set }
 }
 
 public protocol ListViewHeader {
+    var headerClass: AnyClass { get set }
+    var height: CGFloat { get set }
 }
 
 public protocol ListViewFooter {
+    var footerClass: AnyClass { get set }
+    var height: CGFloat { get set }
 }
 
 public protocol ListViewRow {
     var cellClass: AnyClass { get set }
+    var height: CGFloat { get set }
 }
 
 public protocol ListViewUpdate {
